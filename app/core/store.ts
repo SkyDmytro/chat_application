588c1b95094655c0466889fa27c20de249d40922
Dima <skydmytro@gmail.com>
2024-07-10 17:27:04 +0200

    feat: add chats and users redux states

--- app/core/store.ts
+++ app/core/store.ts
@@ -1,4 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-export default configureStore({
-  reducer: {},
+import userReducer from "./reduxSlices/usersSlice";
+import chatsReducer from "./reduxSlices/chatsSlice";
+const store = configureStore({
+  reducer: {
+    users: userReducer,
+    chats: chatsReducer,
+  },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
